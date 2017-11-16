0f952654a4fe37b7fd3254bea69a4eaaef86e7fb
marasolarp <mariasolarp@gmail.com>
2017-11-16 17:39:22 -0300

    nav

--- js/app.js
+++ js/app.js
@@ -3,4 +3,22 @@
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+//console.log(data);
+/*menu hamburgesa*/
+var boton=document.getElementById('btn');
+
+function showMenu(){
+	//guarda en una var nuestro nav
+var menu =document.getElementById('opcs-menu');
+if (menu.classList.contains('disabled-menu')) {// classLIst.contains verifica la lista
+	menu.classList.remove('disabled-menu');
+	menu.classList.add('enable-menu')
+} 
+else {
+	menu.classList.remove('enable-menu');
+	menu.classList.add('disabled-menu');
+	console.log('ocultar');
+}
+
+}
+boton.addEventListener('click',showMenu);
