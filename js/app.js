63e250909e3fa01b5dfe11d0205535173fbf4589
IreeRodriguez <ire208@gmail.com>
2017-11-19 16:59:57 -0300

    scores de estudiantes funcionando

--- js/app.js
+++ js/app.js
@@ -26,6 +26,9 @@ function openSection(evt, seccion) {
 window.onload = function () {
 	search(data,'SCL','2017-2','students',true);
 }
+
+
+
 //funcion para buscar dentro de la data la informacion de las students//
 function search(data,city,gen,students,status) {
     var students = data[city][gen][students];
@@ -38,6 +41,8 @@ function search(data,city,gen,students,status) {
 
     for(var i=0; i < students.length; i++){
         if(students[i].active===status){
+            var active = document.getElementById('active');
+            var inactive = document.getElementById('inactive');
 			var scoreTech = 0;
 			var maxTech = 1800 * students[i].sprints.length;
 			var scoreHse = 0;
@@ -48,6 +53,11 @@ function search(data,city,gen,students,status) {
             var divGirl = document.createElement('div');
             var photo = document.createElement('img');
             var cont = document.createElement('div');
+            cont.className='cont';
+			var divTech = document.createElement('div');
+			var divHse = document.createElement('div');
+			divTech.className="score";
+			divHse.className="score";
             photo.setAttribute('src', students[i].photo);
             var pic = document.createElement('div');
             divGirl.classList.add('girl');
@@ -58,7 +68,12 @@ function search(data,city,gen,students,status) {
             divGirl.appendChild(cont);
             divStudents.appendChild(divGirl);
 
+
 			if (students[i].active===true) {
+                active.classList.add('active');
+                inactive.classList.remove('active');
+
+
 				for(var j=0 ; j < students[i].sprints.length;j++){
 					scoreTech += parseInt(students[i].sprints[j].score.tech);
 					scoreHse += parseInt(students[i].sprints[j].score.hse);
@@ -70,23 +85,41 @@ function search(data,city,gen,students,status) {
 				var hseFinal = scoreHse * 100 / maxHse;
 				// console.log(hseFinal);
 				// console.log(techFinal);
-				var tech = document.createElement('p');
-				var hse = document.createElement('p');
-				tech.textContent = techFinal.toFixed(2);
-				hse.textContent = hseFinal.toFixed(2);
-				cont.appendChild(tech);
-				cont.appendChild(hse);
+				var tech = document.createElement('h4');
+				var hse = document.createElement('h4');
+				var techTitle = document.createElement('p');
+				var hseTitle = document.createElement('p');
+				techTitle.textContent = 'Teck Skills';
+				hseTitle.textContent = 'Life Skills';
+				tech.textContent = techFinal.toFixed(2)+ '%';
+				hse.textContent = hseFinal.toFixed(2)+ '%';
+				divTech.appendChild(tech);
+				divTech.appendChild(techTitle);
+				divHse.appendChild(hse);
+				divHse.appendChild(hseTitle);
+				cont.appendChild(divTech);
+				cont.appendChild(divHse);
 			} else{
+                inactive.classList.add('active');
+                active.classList.remove('active');
 				var techFinal = 0;
 				var hseFinal = 0;
 				// console.log(hseFinal);
 				// console.log(techFinal);
-				var tech = document.createElement('p');
-				var hse = document.createElement('p');
-				tech.textContent = techFinal.toFixed(2);
-				hse.textContent = hseFinal.toFixed(2);
-				cont.appendChild(tech);
-				cont.appendChild(hse);
+				var tech = document.createElement('h4');
+				var hse = document.createElement('h4');
+				tech.textContent = techFinal + '%';
+				hse.textContent = hseFinal + '%' ;
+				var techTitle = document.createElement('p');
+				var hseTitle = document.createElement('p');
+				techTitle.textContent = 'Teck Skills';
+				hseTitle.textContent = 'Life Skills';
+				divTech.appendChild(tech);
+				divTech.appendChild(techTitle);
+				divHse.appendChild(hse);
+				divHse.appendChild(hseTitle);
+				cont.appendChild(divTech);
+				cont.appendChild(divHse);
 
 			}
 
