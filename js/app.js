066b258290b2a461f058c3135a27339384c17ce3
IreeRodriguez <ire208@gmail.com>
2017-11-19 13:29:20 -0300

    scores de estudiantes

--- js/app.js
+++ js/app.js
@@ -4,24 +4,6 @@
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 // console.log(data[AQP]);
-/*menu hamburgesa*/
-var boton=document.getElementById('btn');
-
-function showMenu(){
-	//guarda en una var nuestro nav
-	var menu =document.getElementById('opcs-menu');
-	if (menu.classList.contains('disabled-menu')) {// classLIst.contains verifica la lista
-		menu.classList.remove('disabled-menu');
-		menu.classList.add('enable-menu')
-	}
-	else {
-		menu.classList.remove('enable-menu');
-		menu.classList.add('disabled-menu');
-		console.log('ocultar');
-	}
-
-}
-boton.addEventListener('click',showMenu);
 
 
 //funcionalidad para le menu de tabs//
@@ -41,10 +23,13 @@ function openSection(evt, seccion) {
     evt.currentTarget.className += " active";
 }
 
-
+window.onload = function () {
+	search(data,'SCL','2017-2','students',true);
+}
 //funcion para buscar dentro de la data la informacion de las students//
 function search(data,city,gen,students,status) {
     var students = data[city][gen][students];
+	// console.log(students[0].sprints.length);
 
     var divStudents= document.getElementById('datagirls');
     while (divStudents.hasChildNodes()) {
@@ -53,6 +38,12 @@ function search(data,city,gen,students,status) {
 
     for(var i=0; i < students.length; i++){
         if(students[i].active===status){
+			var scoreTech = 0;
+			var maxTech = 1800 * students[i].sprints.length;
+			var scoreHse = 0;
+			var maxHse = 1200 * students[i].sprints.length;
+			// console.log(maxHse);
+			// console.log(maxTech);
             var textStudents = document.createElement('h3');
             var divGirl = document.createElement('div');
             var photo = document.createElement('img');
@@ -66,6 +57,40 @@ function search(data,city,gen,students,status) {
             cont.appendChild(textStudents);
             divGirl.appendChild(cont);
             divStudents.appendChild(divGirl);
+
+			if (students[i].active===true) {
+				for(var j=0 ; j < students[i].sprints.length;j++){
+					scoreTech += parseInt(students[i].sprints[j].score.tech);
+					scoreHse += parseInt(students[i].sprints[j].score.hse);
+					// console.log(scoreHse);
+					// console.log(scoreTech);
+				}
+
+				var techFinal = scoreTech * 100 / maxTech;
+				var hseFinal = scoreHse * 100 / maxHse;
+				// console.log(hseFinal);
+				// console.log(techFinal);
+				var tech = document.createElement('p');
+				var hse = document.createElement('p');
+				tech.textContent = techFinal.toFixed(2);
+				hse.textContent = hseFinal.toFixed(2);
+				cont.appendChild(tech);
+				cont.appendChild(hse);
+			} else{
+				var techFinal = 0;
+				var hseFinal = 0;
+				// console.log(hseFinal);
+				// console.log(techFinal);
+				var tech = document.createElement('p');
+				var hse = document.createElement('p');
+				tech.textContent = techFinal.toFixed(2);
+				hse.textContent = hseFinal.toFixed(2);
+				cont.appendChild(tech);
+				cont.appendChild(hse);
+
+			}
+
+
         }
 
     };
