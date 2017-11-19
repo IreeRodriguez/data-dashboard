2b201469c6ff0298094e086fcff3106952119e5e
IreeRodriguez <ire208@gmail.com>
2017-11-19 12:11:05 -0300

    funcionalidad para que solo muestre activas o inactvas sin que se repitan

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ function showMenu(){
 boton.addEventListener('click',showMenu);
 
 
-
+//funcionalidad para le menu de tabs//
 function openSection(evt, seccion) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -39,4 +39,34 @@ function openSection(evt, seccion) {
     }
     document.getElementById(seccion).style.display = "block";
     evt.currentTarget.className += " active";
+}
+
+
+//funcion para buscar dentro de la data la informacion de las students//
+function search(data,city,gen,students,status) {
+    var students = data[city][gen][students];
+
+    var divStudents= document.getElementById('datagirls');
+    while (divStudents.hasChildNodes()) {
+        divStudents.removeChild(divStudents.lastChild);
+    }
+
+    for(var i=0; i < students.length; i++){
+        if(students[i].active===status){
+            var textStudents = document.createElement('h3');
+            var divGirl = document.createElement('div');
+            var photo = document.createElement('img');
+            var cont = document.createElement('div');
+            photo.setAttribute('src', students[i].photo);
+            var pic = document.createElement('div');
+            divGirl.classList.add('girl');
+            pic.appendChild(photo);
+            textStudents.textContent = students[i].name;
+            divGirl.appendChild(pic);
+            cont.appendChild(textStudents);
+            divGirl.appendChild(cont);
+            divStudents.appendChild(divGirl);
+        }
+
+    };
 }
--- js/data.js
+++ js/data.js
@@ -5132,39 +5132,3 @@ var arequipa = 'AQP';
 var mexico = 'CDMX';
 var lima = 'LIM';
 var santiago = 'SCL';
-// var search = data[santiago]['2017-2']['students'];
-
-// console.log(search);
-// window.onload = function() {
-//     document.getElementById('active').checked = true;
-//
-// }
-
-
-function search(data,city,gen,students,status) {
-    var students = data[city][gen][students];
-
-    var divStudents= document.getElementById('datagirls');
-
-    for(var i=0; i < students.length; i++){
-        if(students[i].active===status){
-            var textStudents = document.createElement('h3');
-            var divGirl = document.createElement('div');
-            var photo = document.createElement('img');
-            var cont = document.createElement('div');
-            photo.setAttribute('src', students[i].photo);
-            var pic = document.createElement('div');
-            divGirl.classList.add('girl');
-            pic.appendChild(photo);
-            textStudents.textContent = students[i].name;
-            divGirl.appendChild(pic);
-            cont.appendChild(textStudents);
-            divGirl.appendChild(cont);
-            divStudents.appendChild(divGirl);
-        }
-
-    };
-}
-
-
-// var prueba = search(data,'SCL','2017-2','students',true);
